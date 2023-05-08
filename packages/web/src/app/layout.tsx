f9a5e9839e89ccfe6b920f24822bb3fe363d5b8c
Evan Fleet <evfleet@gmail.com>
2023-05-08 12:32:49 -0400

    chore(web): :construction: setup frontend boilerplate components (#4)

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -1,4 +1,5 @@
 import "./globals.css";
+import TopBar from "@/components/TopBar/TopBar";
 
 export const metadata = {
 	title: "Create Next App",
@@ -12,7 +13,10 @@ const Layout = ({
 }) => {
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<body>
+				<TopBar />
+				{children}
+			</body>
 		</html>
 	);
 };
